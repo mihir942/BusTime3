be3ade9f262c44c2a3f39bad7ed3a708c8101ee5
Mihir <mihirniyogi942@gmail.com>
2022-04-10 14:44:05 +0800

    BottomNavBar now has functionality

---
+++ app/src/main/java/com/pmapps/bustime3/FavFragment.java
@@ -0,0 +1,22 @@
+package com.pmapps.bustime3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FavFragment extends Fragment {
+
+    public FavFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fav, container, false);
+    }
+}
--- app/src/main/java/com/pmapps/bustime3/MainActivity.java
+++ app/src/main/java/com/pmapps/bustime3/MainActivity.java
@@ -1,14 +1,46 @@
 package com.pmapps.bustime3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(listener);
+
+        // at the start, goes to the Fav Page automatically
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new FavFragment()).commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener listener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.fav_page_1: selectedFragment = new FavFragment(); break;
+                case R.id.search_page_2: selectedFragment = new SearchFragment(); break;
+                case R.id.nearby_page_3: selectedFragment = new NearbyFragment(); break;
+            }
+
+            if (selectedFragment != null) getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, selectedFragment).commit();
+            return true;
+        }
+    };
 }
