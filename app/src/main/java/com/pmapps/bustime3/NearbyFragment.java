98d82ababf776ecfe4396e6f84ede7672bbea6e5
Mihir <mihirniyogi942@gmail.com>
2022-04-12 16:48:13 +0800

    google map implemented

--- app/src/main/java/com/pmapps/bustime3/NearbyFragment.java
+++ app/src/main/java/com/pmapps/bustime3/NearbyFragment.java
@@ -2,18 +2,37 @@ package com.pmapps.bustime3;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NearbyFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class NearbyFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nearby, container, false);
+        View v = inflater.inflate(R.layout.fragment_nearby, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+        return v;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng home = new LatLng(1.3266996,103.9008272);
+        googleMap.addMarker(new MarkerOptions().position(home).title("Home marker"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(home,17.0F));
     }
 }
