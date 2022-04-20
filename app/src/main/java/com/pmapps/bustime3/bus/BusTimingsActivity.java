dd84d99b69d0fc264072f857a847bd0b80767424
Mihir <mihirniyogi942@gmail.com>
2022-04-20 17:32:10 +0800

    created bus timings activity

---
+++ app/src/main/java/com/pmapps/bustime3/bus/BusTimingsActivity.java
@@ -0,0 +1,16 @@
+package com.pmapps.bustime3.bus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pmapps.bustime3.R;
+
+public class BusTimingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_timings);
+    }
+}
