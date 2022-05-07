f0fb89c65e5ddd0412ec918c9849ec6fad3fb1d6
Mihir <mihirniyogi942@gmail.com>
2022-05-07 15:49:47 +0800

    added search title

--- app/src/main/java/com/pmapps/bustime3/fragments/SearchFragment.java
+++ app/src/main/java/com/pmapps/bustime3/fragments/SearchFragment.java
@@ -1,7 +1,9 @@
 package com.pmapps.bustime3.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,14 +14,26 @@ import com.pmapps.bustime3.R;
 
 public class SearchFragment extends Fragment {
 
-    public SearchFragment() {
-        // Required empty public constructor
+    private Context mContext;
+
+    // initialising the context of fragment, for ease of use. no need to call requireContext() all the time.
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+        initialiseStuffs(v);
+        return v;
+    }
+
+    private void initialiseStuffs(View v) {
+
     }
 }
